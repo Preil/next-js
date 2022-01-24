25ce8208d8a2f7951e2f7073e8944baa9465d5e6
Preil <preil2000@gmail.com>
2022-01-24 15:59:38 +0200

    links mapped from objects

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Maxim" },
+    { id: "ilya", name: "ILYA" },
+  ];
   return (
     <div>
       <h1>Clients page list</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
