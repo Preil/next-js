4f680ad9d18cc0dcba93710bdd8c460ae89a965c
Preil <preil2000@gmail.com>
2022-01-25 19:08:04 +0200

    Events list complete

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,10 +1,13 @@
-import Link from "next/link";
-import Button from "../../components/ui/button";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 import classes from "./event-item.module.css";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
-  const humanReadableDate = new Date(date).toLocaleDateString("en-Us", {
+
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
     day: "numeric",
     month: "long",
     year: "numeric",
@@ -19,14 +22,21 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
-          <div clsassName={classes.address}>
+          <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Exlore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
