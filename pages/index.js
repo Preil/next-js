23e234c847623966828c2d97b32f490831a020dc
Preil <preil2000@gmail.com>
2022-01-25 14:06:14 +0200

    Base project pages added

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,6 @@ function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
-      <ul>
-        <li>
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-            <Link href="/clients">Clients</Link>
-        </li>
-      </ul>
     </div>
   );
 }
