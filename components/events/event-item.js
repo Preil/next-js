d7f8ca0e2f0304b7fc6c5e0b50a962c2bcefc916
Preil <preil2000@gmail.com>
2022-01-25 17:27:13 +0200

    Styles and btn component added

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Button from "../../components/ui/button";
+import classes from "./event-item.module.css";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -11,20 +13,20 @@ function EventItem(props) {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={"/" + image} alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div clsassName={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
-          <Link href={exploreLink}>Exlore Event</Link>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>Exlore Event</Button>
         </div>
       </div>
     </li>
