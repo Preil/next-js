35fc512e08f38ec0bac7f6a5b2cbbfe8f9ef06ce
Preil <preil2000@gmail.com>
2022-01-31 07:32:06 +0200

    Update [eventid].js

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -4,6 +4,8 @@ import { getEventById } from "../../dummy-data";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
+
 
 
 function EventDetailPage() {
@@ -14,13 +16,18 @@ function EventDetailPage() {
   console.log(eventId);
 
   if (!event) {
-    return <p>No event found!</p>;
+    return <ErrorAlert>No event found!</ErrorAlert>;
   }
 
   return (
     <Fragment>
-      <EventSummary title={event.title}/>
-      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
