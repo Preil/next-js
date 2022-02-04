2a23dd33493d83181d76af047a15422b3f05d572
Preil <preil2000@gmail.com>
2022-02-04 11:08:58 +0200

    404 and redirect examples

--- pages/index.js
+++ pages/index.js
@@ -16,16 +16,31 @@ function HomePage(props) {
 
 // this function provides data for the page to prerender,
 export async function getStaticProps() {
-  console.log('Regenerating...')
+  console.log("Regenerating...");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  //redirect example incase data can't be fetched for some reasons
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-date",
+      },
+    };
+  }
+
+  // rendering 404 example
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
   return {
     props: {
       products: data.products,
     },
     // revalidate parameter means that page will be regerated at server side every 10 seconds
-    revalidate: 10
+    revalidate: 10,
   };
 }
 
